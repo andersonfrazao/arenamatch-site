919218471fb325eb9683806ae3d5f268fbd467c9
Anderson Frazao <asfrazao@gmail.com>
2026-04-29 09:44:44 -0300

    Primeiro commit

---
+++ src/main/java/br/com/arenamatch/ArenamatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.arenamatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArenamatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArenamatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/arenamatch/ArenamatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arenamatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArenamatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
